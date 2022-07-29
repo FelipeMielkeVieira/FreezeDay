7e5f0c5d975d0a24f3bd4adc51f1de729d1bd95b
MatheusFranzener <felipe_mielke-vieira@estudante.sc.senai.br>
2022-07-29 16:24:09 -0300

    29/07 - 16:24

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+app.use(express.static("public"))
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/home.html"));
+  })
+  
+
+app.listen(3000, () => {
+    console.log('App funcionando na porta 3000')
+});
