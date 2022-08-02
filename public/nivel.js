9a8c6bcaf9868459c9116208e71ce07612f1f7e7
Kenzohfs <88584837+Kenzohfs@users.noreply.github.com>
2022-08-01 21:53:42 -0300

    01/08 - 21:52
    Feito algumas paredes do nivel 1 (R1 -R5)

--- public/nivel.js
+++ public/nivel.js
@@ -15,7 +15,7 @@ loadSprite("playerD", "/sprites/playerD.png");
 loadSprite("redSlimeD", "/sprites/redSlimeD.png");
 
 scene("nivel1", () => {
-    
+
     const borderTop = add([
         "borderTop",
         rect(totalWidth, 10),
@@ -56,7 +56,7 @@ scene("nivel1", () => {
     const redSlime = add([
         "redSlime",
         sprite("redSlimeD"),
-        pos(totalWidth * 0.05, totalHeight * 0.05),
+        pos(totalWidth * 0.03, totalHeight * 0.05),
         area(),
         solid()
     ])
@@ -64,10 +64,60 @@ scene("nivel1", () => {
     const player = add([
         "player",
         sprite("playerD"),
-        pos(totalWidth * 0.05, totalHeight * 0.85),
+        pos(totalWidth * 0.03, totalHeight * 0.85),
         area(),
         solid(),
         scale(1.5)
+    ])
+
+    //R1
+    add([
+        rect(totalWidth * 0.08, totalHeight * 0.15),
+        pos(totalWidth * 0.11,  totalHeight - 10 - (totalHeight * 0.15)),
+        outline(4),
+        area(),
+        solid(),
+        color(127, 200, 255)
+    ])
+
+    //R2
+    add([
+        rect(totalWidth * 0.185, totalHeight * 0.08),
+        pos(0 + 10, totalHeight  * 0.65),
+        outline(4),
+        area(),
+        solid(),
+        color(127, 200, 255)
+    ])
+
+    //R3
+    add([
+        rect(totalWidth * 0.125, totalHeight * 0.13),
+        pos((10 + totalWidth * 0.185) * 0.35, totalHeight  * 0.43),
+        outline(4),
+        area(),
+        solid(),
+        color(127, 200, 255)
+    ])
+
+    //R4
+    add([
+        rect(totalWidth * 0.185, totalHeight * 0.08),
+        pos(0 + 10, totalHeight  * 0.35 - (totalHeight * 0.08)),
+        outline(4),
+        area(),
+        solid(),
+        color(127, 200, 255)
+    ])
+
+    //R5
+    add([
+        rect(totalWidth * 0.14, totalHeight * 0.06),
+        pos(totalWidth * 0.045 + 10, totalHeight  * 0.21 - (totalHeight * 0.08)),
+        outline(4),
+        area(),
+        solid(),
+        color(127, 200, 255)
     ])
 
     onKeyDown("left", () => {
